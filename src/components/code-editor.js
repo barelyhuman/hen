a6005182d476c0ee693c1f6c139964495c5e4b2f
sidgroks@gmail.com <sidgroks@gmail.com>
2020-07-17 12:22:42 +0530

    Addition:Formatter
    Formatter added with prettier in place to format on button click

--- src/components/code-editor.js
+++ src/components/code-editor.js
@@ -3,6 +3,11 @@ import Editor from "react-simple-code-editor";
 import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
+import prettier from "prettier/standalone";
+import parserBabel from "prettier/parser-babel";
+
+import Button from "./button";
+import Spacer from "./spacer";
 
 export default (props) => {
   const [code, setCode] = useState(props.code);
@@ -13,8 +18,19 @@ export default (props) => {
     props.onCodeChange(_value);
   };
 
+  const formatCode = () => {
+    let _value = prettier.format(code, {
+      parser: "babel",
+      plugins: [parserBabel],
+    });
+    setCode(_value);
+    props.onCodeChange(_value);
+  };
+
   return (
     <>
+      <Button onClick={formatCode}>Format</Button>
+      <Spacer y={1} />
       <Editor
         value={code}
         onValueChange={handleValueChange}
