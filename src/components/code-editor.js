dafcdfdc3216dc932508a91e90701bd24c7dceab
sidgroks@gmail.com <sidgroks@gmail.com>
2020-07-17 19:42:39 +0530

    prism dep tree

--- src/components/code-editor.js
+++ src/components/code-editor.js
@@ -1,9 +1,11 @@
 import debounce from "lodash.debounce";
 import parserBabel from "prettier/parser-babel";
 import prettier from "prettier/standalone";
-import "prismjs/components/prism-clike";
+
 import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
+
 import React, { useCallback, useState } from "react";
 import useMousetrap from "react-hook-mousetrap";
 import Editor from "react-simple-code-editor";
