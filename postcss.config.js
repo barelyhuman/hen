fbde53f54b672f45bc1d966db8c8a6d15121bda6
Reaper <sidgroks@gmail.com>
2021-07-22 13:36:13 +0530

    config: add containers to postcss add container nested files to postcss to generate styles based on containers as well

--- postcss.config.js
+++ postcss.config.js
@@ -7,13 +7,13 @@ module.exports = {
             "postcss-preset-env",
             {
               autoprefixer: {
-                flexbox: "no-2009",
+                flexbox: "no-2009"
               },
               stage: 3,
               features: {
-                "custom-properties": false,
-              },
-            },
+                "custom-properties": false
+              }
+            }
           ],
           [
             "@fullhuman/postcss-purgecss",
@@ -21,15 +21,16 @@ module.exports = {
               content: [
                 "./pages/**/*.{js,jsx,ts,tsx}",
                 "./components/**/*.{js,jsx,ts,tsx}",
+                "./containers/**/*.{js,jsx,ts,tsx}"
               ],
               defaultExtractor: (content) =>
                 content.match(/[\w-/:]+(?<!:)/g) || [],
               safelist: {
                 standard: ["html", "body"],
-                deep: [/^hljs/],
-              },
-            },
-          ],
+                deep: [/^hljs/]
+              }
+            }
+          ]
         ]
-      : [],
+      : []
 };
